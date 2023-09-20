7d8847657f4e2743a0e6684e8bd3e9259359561f
Henry Seigbein <hensco95@gmail.com>
2023-09-20 23:16:31 +0100

    made some changes

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -1,6 +1,5 @@
-const { readFile, writeFile } = require("fs");
+const { readFile, writeFile, read } = require("fs");
 
-console.log("start");
 readFile("./content/first.txt", "utf8", (err, result) => {
   if (err) {
     console.log(err);
@@ -23,10 +22,8 @@ readFile("./content/first.txt", "utf8", (err, result) => {
           console.log(err);
           return;
         }
-        console.log("done with this task");
+        console.log(result);
       }
     );
   });
 });
-
-console.log("starting next task");
